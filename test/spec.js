30b758b6e4bfae287d3e2507f51e5122ee8f56f8
kannkyo <15080890+kannkyo@users.noreply.github.com>
2019-04-01 00:01:27 +0900

    Add chai

--- test/spec.js
+++ test/spec.js
@@ -1,12 +1,16 @@
 const Application = require('spectron').Application
-const assert = require('assert')
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
 const electronPath = require('electron')
 const path = require('path')
 
-describe('Application launch', function() {
-  this.timeout(10000)
+chai.should()
+chai.use(chaiAsPromised)
 
-  beforeEach(function() {
+describe('Application launch', function () {
+  this.timeout(10000);
+
+  beforeEach(function () {
     this.app = new Application({
       path: electronPath,
       args: [path.join(__dirname, '..')]
@@ -14,15 +18,23 @@ describe('Application launch', function() {
     return this.app.start()
   })
 
-  afterEach(function() {
+  beforeEach(function () {
+    chaiAsPromised.transferPromiseness = this.app.transferPromiseness
+  })
+
+  afterEach(function () {
     if (this.app && this.app.isRunning()) {
       return this.app.stop()
     }
   })
 
-  it('shows an initial window', function() {
-    return this.app.client.getWindowCount().then(function(count) {
-      assert.equal(count, 1)
-    })
+  it('opens a window', function () {
+    return this.app.client.waitUntilWindowLoaded()
+      .getWindowCount().should.eventually.have.at.least(1)
+      .browserWindow.isMinimized().should.eventually.be.false
+      .browserWindow.isVisible().should.eventually.be.true
+      .browserWindow.isFocused().should.eventually.be.true
+      .browserWindow.getBounds().should.eventually.have.property('width').and.be.above(0)
+      .browserWindow.getBounds().should.eventually.have.property('height').and.be.above(0)
   })
 })
